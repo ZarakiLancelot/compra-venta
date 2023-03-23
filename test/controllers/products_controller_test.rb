6741fddc848afb3f11d32b43c4478d1b09bde207
ZarakiLancelot <kenpachi2783@gmail.com>
2023-03-23 03:30:07 -0600

    test for products

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test 'render a list of all products' do
+    get products_path
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'render a specific product' do
+    get product_path(products(:ps4))
+    assert_response :success
+    assert_select '.name', 'PS4 PRO'
+    assert_select '.description', 'En excelente estado'
+    assert_select '.price', '$. 400'
+  end
+end
