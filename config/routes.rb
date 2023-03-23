51799c905ff0ab49c1052c95f175b81ca3da54f7
ZarakiLancelot <kenpachi2783@gmail.com>
2023-03-23 03:01:10 -0600

    show method of products

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
 end
